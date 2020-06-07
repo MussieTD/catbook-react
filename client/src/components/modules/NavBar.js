a3d8ff116994f250868d43ffa793d8a1fb99e4cd
MussieTD <mustesh5@gmail.com>
2020-06-07 13:20:55 -0400

    small naming changes

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -18,7 +18,7 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="NavBar-container">
-        <div className="NavBar-title u-inlineBlock">Catbook</div>
+        <div className="NavBar-title u-inlineBlock">Meftihe</div>
         <div className="NavBar-linkContainer u-inlineBlock">
           <Link to="/" className="NavBar-link">
             Home
